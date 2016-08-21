0bc8aeef55701f5f8eb46f8a1c5f06a32abf9a80
Francis Bourre <francis_bourre@me.com>
2016-08-21 08:03:49 +0200

    fix for haxe 3.2.1

--- test/hex/compiler/parser/xml/state/StatefulStateMachineConfigTest.hx
+++ test/hex/compiler/parser/xml/state/StatefulStateMachineConfigTest.hx
@@ -1,10 +1,6 @@
 package hex.compiler.parser.xml.state;
 
 import hex.domain.ApplicationDomainDispatcher;
-import hex.ioc.assembler.ApplicationAssembler;
-import hex.ioc.core.IContextFactory;
-import hex.ioc.core.ICoreFactory;
-import hex.ioc.parser.xml.ApplicationXMLParser;
 import hex.ioc.parser.xml.state.mock.MockModuleWorkingWithStates;
 import hex.ioc.parser.xml.state.mock.MockStateEnum;
 import hex.state.State;
@@ -17,44 +13,25 @@ import hex.unittest.assertion.Assert;
  */
 class StatefulStateMachineConfigTest
 {
-	var _contextParser 				: ApplicationXMLParser;
-	var _builderFactory 			: IContextFactory;
-	var _applicationAssembler 		: ApplicationAssembler;
-		
-	@Before
-	public function setUp() : Void
-	{
-		this._applicationAssembler 	= new ApplicationAssembler();
-		this._builderFactory 		= this._applicationAssembler.getContextFactory( this._applicationAssembler.getApplicationContext( "applicationContext" ) );
-	}
-
-	@After
-	public function tearDown() : Void
-	{
-		ApplicationDomainDispatcher.getInstance().clear();
-		this._applicationAssembler.release();
-	}
-	
-	function _getCoreFactory() : ICoreFactory
-	{
-		return this._applicationAssembler.getApplicationContext( "applicationContext" ).getCoreFactory();
-	}
-	
 	@Test( "test statemachine configuration" )
 	public function testStateMachineConfiguration() : Void
 	{
-		this._applicationAssembler = XmlCompiler.readXmlFile( "context/statefulStateMachineConfigTest.xml" );
+		var assembler = XmlCompiler.readXmlFile( "context/statefulStateMachineConfigTest.xml" );
+		var factory = assembler.getApplicationContext( "applicationContext" ).getCoreFactory();
 
-		var initialState : State = this._getCoreFactory().locate( "initialState" );
+		var initialState : State = factory.locate( "initialState" );
 		Assert.isNotNull( initialState, "state should not be null" );
 		Assert.equals( MockStateEnum.INITIAL_STATE, initialState, "state should be the same" );
 
-		var stateConfig : StatefulStateMachineConfig = this._getCoreFactory().locate( "stateConfig" );
+		var stateConfig : StatefulStateMachineConfig = factory.locate( "stateConfig" );
 		Assert.isNotNull( stateConfig, "config should not be null" );
 
-		var myModule : MockModuleWorkingWithStates = this._getCoreFactory().locate( "myModule" );
+		var myModule : MockModuleWorkingWithStates = factory.locate( "myModule" );
 		Assert.isNotNull( myModule, "module should not be null" );
 
 		Assert.isTrue( myModule.commandWasCalled, "command should be called" );
+		
+		ApplicationDomainDispatcher.getInstance().clear();
+		assembler.release();
 	}
 }
