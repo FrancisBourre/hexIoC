19e33821f7940f3eeaab7f02889e3fa68b94a09e
Francis Bourre <francis_bourre@me.com>
2016-08-21 18:35:00 +0200

    LightApplicationContext class added for generating applications without state machine

---
+++ src/hex/ioc/assembler/LightApplicationContext.hx
@@ -0,0 +1,22 @@
+package hex.ioc.assembler;
+
+import hex.event.MessageType;
+import hex.ioc.core.ICoreFactory;
+
+/**
+ * ...
+ * @author Francis Bourre
+ */
+class LightApplicationContext extends AbstractApplicationContext
+{
+	@:allow( hex.ioc.core )
+	function new( coreFactory : ICoreFactory, name : String )
+	{
+		super( coreFactory, name );
+	}
+	
+	override public function dispatch( messageType : MessageType, ?data : Array<Dynamic> ) : Void
+	{
+		//do ntohing
+	}
+}
