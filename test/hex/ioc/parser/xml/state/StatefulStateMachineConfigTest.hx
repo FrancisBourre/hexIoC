2fdf18a91269ca7f77e2c67a8f4652d85541a364
Francis Bourre <francis_bourre@me.com>
2016-08-21 08:19:48 +0200

    fix for travis with haxe version < 3.3

--- test/hex/ioc/parser/xml/state/StatefulStateMachineConfigTest.hx
+++ test/hex/ioc/parser/xml/state/StatefulStateMachineConfigTest.hx
@@ -1,8 +1,5 @@
 package hex.ioc.parser.xml.state;
 
-import hex.domain.ApplicationDomainDispatcher;
-import hex.ioc.assembler.ApplicationAssembler;
-import hex.ioc.core.IContextFactory;
 import hex.ioc.parser.xml.state.mock.MockModuleWorkingWithStates;
 import hex.ioc.parser.xml.state.mock.MockStateEnum;
 import hex.state.State;
@@ -16,32 +13,25 @@ import hex.unittest.assertion.Assert;
  */
 class StatefulStateMachineConfigTest
 {
-	var _builderFactory 			: IContextFactory;
-	var _applicationAssembler 		: ApplicationAssembler;
-
-	@After
-	public function tearDown() : Void
-	{
-		ApplicationDomainDispatcher.getInstance().clear();
-		this._applicationAssembler.release();
-	}
-	
 	@Test( "test statemachine configuration" )
 	public function testStateMachineConfiguration() : Void
 	{
-		this._applicationAssembler 	= XmlReader.readXmlFile( "context/statefulStateMachineConfigTest.xml" );
-		this._builderFactory 		= this._applicationAssembler.getContextFactory( this._applicationAssembler.getApplicationContext( "applicationContext" ) );
+		//there's a bug with haxe version < 3.3 with recursive toJSon call
+		#if (haxe_ver >= "3.3")
+		var assembler = XmlReader.readXmlFile( "context/statefulStateMachineConfigTest.xml" );
+		var factory	= assembler.getApplicationContext( "applicationContext" ).getCoreFactory();
 
-		var initialState : State = this._builderFactory.getCoreFactory().locate( "initialState" );
+		var initialState : State = factory.locate( "initialState" );
 		Assert.isNotNull( initialState, "state should not be null" );
 		Assert.equals( MockStateEnum.INITIAL_STATE, initialState, "state should be the same" );
 
-		var stateConfig : StatefulStateMachineConfig = this._builderFactory.getCoreFactory().locate( "stateConfig" );
+		var stateConfig : StatefulStateMachineConfig = factory.locate( "stateConfig" );
 		Assert.isNotNull( stateConfig, "config should not be null" );
 
-		var myModule : MockModuleWorkingWithStates = this._builderFactory.getCoreFactory().locate( "myModule" );
+		var myModule : MockModuleWorkingWithStates = factory.locate( "myModule" );
 		Assert.isNotNull( myModule, "module should not be null" );
 
 		Assert.isTrue( myModule.commandWasCalled, "command should be called" );
+		#end
 	}
 }
