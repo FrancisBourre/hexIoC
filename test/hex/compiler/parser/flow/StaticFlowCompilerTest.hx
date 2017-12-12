648d0504f14193662c59129c0d9f4954179cc6b9
Francis Bourre <francis_bourre@me.com>
2017-12-12 10:04:41 +0100

    Test fix for static platforms

--- test/hex/compiler/parser/flow/StaticFlowCompilerTest.hx
+++ test/hex/compiler/parser/flow/StaticFlowCompilerTest.hx
@@ -1569,8 +1569,8 @@ class StaticFlowCompilerTest
 		
 		Assert.equals( 'isPublic', code.locator.isPublic );
 		Assert.equals( 'isPrivate', code.locator.wasPrivate );
-		Assert.equals( 3, code.locator.isLazy );
-		Assert.equals( 4, code.locator.wasPrivateAndLazy );
+		Assert.equals( '3', code.locator.isLazy );
+		Assert.equals( '4', code.locator.wasPrivateAndLazy );
 		//Assert.equals( 4, code.locator.isPrivateAndLazy );
 	}
 	
